ce0df489b85a5d1b2897056e55443f21613399e7
AlbertBi7 <161057291+AlbertBi7@users.noreply.github.com>
2024-04-09 21:24:57 +0530

    commit

--- arrayBubblesort.c
+++ arrayBubblesort.c
@@ -1,5 +1,28 @@
 #include <stdio.c>
 int main(){
     int i,n,pass,temp;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+
+    printf("Enter the elements:");
+    for(i=0;i<n;i++){
+        scanf("%d",&myArray[i]);
+    }
+
+    for(pass=0;pass<n;pass++){
+        for(i=0;i<n-pass-1;i++){
+            if(myArray[i]>myArray[i+1]){
+                temp=myArray[i];
+                myArray[i]=myArray[i+1];
+                myArray[i+1]=temp
+            }
+        }
+    }
+    printf("The sorted array is:");
+    for(i=0;i<n;i++){
+        printf("%d",myArray[i]);
+    }
     return 0;
 }
+
+
